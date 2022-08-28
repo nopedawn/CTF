deb9bd7ce4e7826fbb560b3b9e19abfec139bc3d
nopedawn <62634724+nopedawn@users.noreply.github.com>
2022-08-28 04:38:38 -0400

    added "5 files"

---
+++ Compfest14/HackerClass/Reverse Engineering/Binary Pin/Binary-Pin decompiled/Binarypin.java
@@ -0,0 +1,38 @@
+/*
+ * Decompiled with CFR 0.150.
+ */
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+public class Binarypin {
+    private JFrame frame = new JFrame("Binary Pin");
+    private JLabel output = new JLabel();
+
+    Binarypin() {
+        this.output.setBounds(20, 250, 360, 20);
+        JLabel jLabel = new JLabel("Your data:");
+        jLabel.setBounds(20, 235, 100, 20);
+        this.frame.add(jLabel);
+        this.frame.add(this.output);
+        this.frame.add(new PinButton(this, "0", 43, 10, 150, 150));
+        this.frame.add(new PinButton(this, "1", 198, 10, 150, 150));
+        this.frame.add(new ResetButton(this, 43, 165, 305, 50));
+        this.frame.setSize(400, 350);
+        this.frame.setLayout(null);
+        this.frame.setVisible(true);
+        this.frame.setDefaultCloseOperation(3);
+    }
+
+    public static void main(String[] arrstring) {
+        new Binarypin();
+    }
+
+    public void clearOutput() {
+        this.output.setText("");
+    }
+
+    public void updateOutput() {
+        this.output.setText(Secret.getInstance().getData());
+    }
+}
+
