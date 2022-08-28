00fd54058b15283a44a745b3064fd642e7e4541d
nopedawn <62634724+nopedawn@users.noreply.github.com>
2022-08-28 04:43:39 -0400

    added "submit.php'

---
+++ Compfest14/HackerClass/Web Exploitation/PacilTxT/submit.php
@@ -0,0 +1,20 @@
+<?php
+require_once('koneksi.php');
+require_once('utils.php');
+
+if (!isset($_POST['title']) || !isset($_POST['content'])) {
+    die('Method not allowed');
+} 
+
+$txtCode = gen_random_string(6);
+$saveTitle = $db_connect->real_escape_string($_POST['title']);
+$txtTitle = substr($saveTitle,0,20);
+$txtContent = $db_connect->real_escape_string($_POST['content']);
+
+$query = "INSERT INTO paciltxt_post(code, title, content) VALUES ('".$txtCode."', '".$txtTitle."', '".$txtContent."')";
+$sql = $db_connect->query($query);
+if ($db_connect->error) {
+    header('Location: /');
+}
+
+header('Location: /view.php?code='.$txtCode);
