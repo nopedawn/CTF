c38c3b8b89afe2392dae577dbaec1aa72a137ea3
nopedawn <62634724+nopedawn@users.noreply.github.com>
2022-09-18 17:40:11 +0700

    Delete xor_solver.py

--- HealthkathonBPJS-2022/xor_solver.py
+++
@@ -1,7 +0,0 @@
-def decrypt(encrypted: bytes, key: bytes):
-    result = []
-    
-    for i in range(len(encrypted)):
-        result.append(encrypted[i] ^ key[i % len(key)])
-
-    return bytes(result)
